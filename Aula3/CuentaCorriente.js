ad12c79b9902bf27655b1c6a4d411db5e25c41f4
Wilmer Gulcochia <karlwgs1989@gmail.com>
2023-04-09 21:04:21 -0500

    Aula3 completada - Clases abstractas

---
+++ Aula3/CuentaCorriente.js
@@ -0,0 +1,22 @@
+import { Cliente } from "./Cliente.js";
+import { Cuenta } from "./Cuenta.js";
+
+export class CuentaCorriente extends Cuenta {
+    static cantidadCuentas = 0;
+
+    constructor(cliente, numero, agencia) {
+        super(cliente, numero, agencia, 0)
+        CuentaCorriente.cantidadCuentas++;
+    }
+
+    retirarDeCuenta(valor) {
+        super._retirarDeCuenta(valor, 5);
+    }
+
+    prueba() {
+        super.prueba();
+        console.log("Método hijo");
+    }
+
+
+}
---
+++ Aula3/index.js
@@ -0,0 +1,21 @@
+/*Importación de clases*/
+import {Cliente} from './Cliente.js'
+import {CuentaCorriente} from './CuentaCorriente.js';
+import { CuentaNomina } from './CuentaNomina.js';
+import { Cuenta } from './Cuenta.js';
+import {CuentaAhorro} from './cuentaAhorro.js';
+
+const cliente = new Cliente('Leonardo','13804050','123224');
+const cliente2 = new Cliente('María','16979808','8989');
+
+const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
+
+const cuentaAhorroLeonardo = new CuentaAhorro(cliente,'9985','001',0);
+
+const cuentaNominaLeonardo = new CuentaNomina (cliente, "9854", "001", 100);
+cuentaNominaLeonardo.depositoEnCuenta(150);
+console.log(cuentaNominaLeonardo.verSaldo());
+
+cuentaNominaLeonardo.retirarDeCuenta( 50);
+console.log(cuentaNominaLeonardo.verSaldo());
